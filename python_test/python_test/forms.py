64eaa18644799f5239e4adea8e0ccfc993afae28
Nija <nijap@techversantinfotech.com>
2019-05-24 16:01:23 +0530

    first commit

---
+++ python_test/python_test/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Client
+
+
+class ClientAddForm(forms.ModelForm):
+    street_name = forms.CharField(required=False)
+    suburb = forms.CharField(required=False)
+    postcode = forms.CharField(required=False)
+    state = forms.CharField(required=False)
+    client_contact_name = forms.CharField(required=True)
+
+    class Meta:
+        model = Client
+        fields = ('client_name', 'street_name', 'suburb', 'postcode', 'state', 'client_contact_name', 'email',
+                  'phone_number',)
